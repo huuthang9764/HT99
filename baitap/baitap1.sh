66f9e124e586bd5d4d3bdc5befb89031869616fa
huuthang <huuthang9764@gmail.com>
2020-12-23 15:05:52 +0700

    https://github.com/huuthang9764/pmnm

--- baitap/baitap1.sh
+++ baitap/baitap1.sh
@@ -1,11 +1,9 @@
-#! /bin/bash
-echo "nhap 1 so nguyen n:"
-if ($n > 0)
-then
-	read n
-	for ((c=1;c<=$n;c++))
-		do
-		sudo useradd -m -d /home/"USER_"$c -s /bin/bash "USER_"$c
-		sudo passwd "USER_"$c <<<"123456"$'\n'"123456"
-	done
-fi
+#!/bin/bash
+echo "Nhap mot so nguyen n:"
+read n
+
+for((i=1;i<=n;i++));
+do
+sudo useradd USER_"$i"
+echo "USER_"$i":123456" | sudo chpasswd
+done
